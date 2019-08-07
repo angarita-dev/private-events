a04129b5778827d809716ac99ed2a115e5d74370
Carlos Sol <fersolmarin@hotmail.com>
2019-08-07 16:22:59 -0500

    Event's Date Column Generated

--- db/migrate/20190806201503_remove_date_and_add_association.rb
+++ db/migrate/20190806201503_remove_date_and_add_association.rb
@@ -1,7 +1,5 @@
 class RemoveDateAndAddAssociation < ActiveRecord::Migration[5.2]
   def change
     add_reference :events, :creator, foreign_key: { to_table: :users }
-    remove_index :events, :created_at
-    remove_column :events, :date, :datetime
   end
 end
