149b2cf390d60e03dce39881aca31e58155dfbbd
codingAngarita <44899916+codingAngarita@users.noreply.github.com>
2019-08-07 12:54:51 -0500

    Added index to display all events

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :users, only: [:new, :create, :show]
   resources :sessions, only: [:new, :create, :destroy]
-  resources :events, only: [:new, :show, :destroy, :edit, :update]
-  root "sessions#new"
+  resources :events
+  root "events#index"
 end
