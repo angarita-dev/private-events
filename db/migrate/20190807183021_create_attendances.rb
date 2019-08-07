a6504f5838a71010410d7aaeb69f789b82799110
codingAngarita <44899916+codingAngarita@users.noreply.github.com>
2019-08-07 15:40:12 -0500

    Attendees association done

---
+++ db/migrate/20190807183021_create_attendances.rb
@@ -0,0 +1,10 @@
+class CreateAttendances < ActiveRecord::Migration[5.2]
+  def change
+    create_table :attendances do |t|
+      t.references :attended_event, foreign_key: { to_table: :events }
+      t.references :event_attendee, foreign_key: { to_table: :users }
+
+      t.timestamps
+    end
+  end
+end
