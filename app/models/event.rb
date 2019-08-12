d6b24b7689ab6f238a56ba9b09c8f3543fca5b42
codingAngarita <44899916+codingAngarita@users.noreply.github.com>
2019-08-12 11:38:49 -0500

    Changes in foreing keys & no logic in helpers

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,9 +1,9 @@
 class Event < ApplicationRecord
-  belongs_to :creator, foreign_key: :creator_id, class_name: :User
+  belongs_to :creator, class_name: :User
 
   has_many :attendances, foreign_key: :attended_event_id
   has_many :attendees, through: :attendances, source: :event_attendee
 
-  scope :previous_events, -> { where("event_date < :current_date", current_date: Date.current) }
-  scope :upcomming_events, -> { where("event_date >= :current_date", current_date: Date.current) }
+  scope :previous_events, -> { where('event_date < ?', Date.current) }
+  scope :upcomming_events, -> { where('event_date >= ?', Date.current) }
 end
