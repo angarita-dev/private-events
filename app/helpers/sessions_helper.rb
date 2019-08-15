9a68d91471093216c0d2b9053a1a1b187bb27993
codingAngarita <44899916+codingAngarita@users.noreply.github.com>
2019-08-15 11:30:46 -0500

    Added session helper missing method

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -2,4 +2,8 @@ module SessionsHelper
   def sign_in?
     !current_user.nil?
   end
+
+  def current_user
+    @current_user ||= User.find_by(remember_token: cookies.signed.permanent[:remember_token])
+  end
 end
