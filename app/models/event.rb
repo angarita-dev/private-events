19c3b05b9d91d73a952d6fd82c3803ef31bc53a7
codingAngarita <44899916+codingAngarita@users.noreply.github.com>
2019-08-15 15:06:28 -0500

    event rel attendees change

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,8 +1,11 @@
 class Event < ApplicationRecord
   belongs_to :creator, class_name: :User
 
-  has_many :attendances, foreign_key: :attended_event_id
-  has_many :attendees, through: :attendances, source: :event_attendee
+  has_many :attendances, foreign_key: :event_id
+  has_many :attendees, through: :attendances,
+                       source: :user,
+                       foreign_key: :event_id , 
+                       class_name: :User
 
   scope :previous_events, -> { where('event_date < ?', Date.current) }
   scope :upcomming_events, -> { where('event_date >= ?', Date.current) }
